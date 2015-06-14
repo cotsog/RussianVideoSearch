a5e67c2c46145a0388e20c07637b916fb8112028
IgorGanapolsky <eazyigz@gmail.com>
2015-06-14 11:39:21 -0400

    Update travis.yml. Start creation of unit tests.

---
+++ Application/src/main/java/com/eazyigz/RussiaMediaSearch/Calculator.java
@@ -0,0 +1,23 @@
+package com.eazyigz.RussiaMediaSearch;
+
+/**
+ * Created by Igor on 6/10/2015.
+ */
+public class Calculator {
+
+    public double sum(double a, double b) {
+        return a + b;
+    }
+
+    public double subtract(double a, double b) {
+        return a - b;
+    }
+
+    public double divide(double a, double b) {
+        return a / b;
+    }
+
+    public double multiply(double a, double b) {
+        return a * b;
+    }
+}
