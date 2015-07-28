93d5b97439834248d644d490565d3441ccf5f23f
IgorGanapolsky <eazyigz@gmail.com>
2015-07-28 12:12:36 -0400

    Circle-ci fix.

--- environmentSetup.sh
+++ environmentSetup.sh
@@ -13,8 +13,7 @@ function getAndroidSDK(){
     echo y | android update sdk -u -a -t build-tools-22.0.1 &&
     echo y | android update sdk -u -a -t sys-img-x86-android-22 &&
     echo y | android update sdk -u -a -t addon-google_apis-google-22 &&
-    echo no | android create avd -n testAVD -f -t android-22 --abi default/x86 &&
-    touch $DEPS
+    echo no | android create avd -n testAVD -f -t android-22 --abi default/x86 && touch $DEPS
   fi
 }
 
