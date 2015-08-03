0ccd52ef2ffaaf170459edebe3362f753227dfa6
IgorGanapolsky <eazyigz@gmail.com>
2015-08-03 11:39:59 -0400

    Cache directories for Travis. Accept all licenses.

--- scripts/environmentSetup.sh
+++ scripts/environmentSetup.sh
@@ -14,7 +14,6 @@ function getAndroidSDK(){
     echo y | android update sdk -u -a -t extra-android-m2repository &&
     echo y | android update sdk -u -a -t extra-google-m2repository &&
     echo y | android update sdk -u -a -t sys-img-x86-android-22 &&
-    echo y | android update sdk -u -a -t addon-google_apis-google-22 &&
     echo no | android create avd -n testAVD -f -t android-22 --abi default/x86 && touch $DEPS
   fi
 }
