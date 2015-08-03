d28aa778e0ad53b2bd5f52e735ff96ca4dd262f2
IgorGanapolsky <eazyigz@gmail.com>
2015-08-03 12:13:11 -0400

    - extra-android-m2repository

--- scripts/environmentSetup.sh
+++ scripts/environmentSetup.sh
@@ -11,8 +11,8 @@ function getAndroidSDK(){
     echo y | android update sdk -u -a -t android-22 &&
     echo y | android update sdk -u -a -t platform-tools &&
     echo y | android update sdk -u -a -t build-tools-22.0.1 &&
-    echo y | android update sdk -u -a -t extra-android-m2repository &&
-    echo y | android update sdk -u -a -t extra-google-m2repository &&
+    echo y | android update sdk -u -a -t extra-android-support &&
+        echo y | android update sdk -u -a -t extra-android-m2repository &&
     echo y | android update sdk -u -a -t sys-img-x86-android-22 &&
     echo no | android create avd -n testAVD -f -t android-22 --abi default/x86 && touch $DEPS
   fi
